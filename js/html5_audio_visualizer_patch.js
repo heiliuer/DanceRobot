b6ce07cea646ba9bc2fbb10dffd979d83b5e55f3
heiliuer <heiliuer@github.com>
2016-07-01 15:47:43 +0800

    fixed

--- js/html5_audio_visualizer_patch.js
+++ js/html5_audio_visualizer_patch.js
@@ -1,16 +1,1 @@
-/**
- * Created by Administrator on 2016/3/17 0017.
- */
-+function () {
-    var ptype = Visualizer.prototype;
-    var ori_start = ptype._start;
-    ptype._start = function () {
-        ori_start.apply(this, arguments);
-    };
-    ptype.setHandlerBuffer = function (handler) {
-        if (!$.isFunction(handler)) {
-            handler = null;
-        }
-        this.handlerBuffer = handler;
-    }
-}();
++function(){var t=Visualizer.prototype,n=t._start;t._start=function(){n.apply(this,arguments)},t.setHandlerBuffer=function(t){$.isFunction(t)||(t=null),this.handlerBuffer=t}}();
